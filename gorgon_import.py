f7225401dcdc807a3533914e5a90213b9037240c
lm2410 <lm2410@ic.ac.uk>
2017-11-22 13:29:37 +0000

    Added vti scalar export
    This will write 3D numpy arrays to the vti file format, for viewing in Paraview.

--- gorgon_import.py
+++ gorgon_import.py
@@ -130,3 +130,48 @@ class gorgon_sim:
                ylim=[self.y[0], self.y[-1]],
                zlim=[self.z[0], self.z[-1]])
         ax.set_aspect(1)
+
+def write_vti_scalar(sim, arr, x=sim.x, y=sim.y, z=sim.z, name=None, output_dir=''):
+    ''' 
+    Writes a 3D numpy array into a VTI file. Only works for scalar values. Must be uniform spacing.
+    arr: the data you want to write.
+        If str: writes from sim.arr
+        If numpy array: writes the numpy array. Assumes the same size as sim unless x, y and z are specified
+        If dict: writes multiple numpy arrays in dict to vti. Assumes the same size as sim unless x, y and z are specified
+    x, y, z: the x, y and z position array,
+    name: Name of the file/data array. Needed for np.array or dict arr input
+    output_dir: Specify a different output folder.    
+    '''
+    from evtk.hl import imageToVTK 
+    if(output_dir is not ''):
+        output_dir += '/'
+    print(output_dir)
+    
+    # If x, y, and z are different from the array
+    xc = np.array([x[0], y[0], z[0]])
+    d = np.array([x[1]-x[0], y[1]-y[0], z[1]-z[0]])
+    
+    if(type(arr) is type('')):        
+        imageToVTK(output_dir+sim.index+'_'+arr,
+                   cellData={arr: sim.arr[arr]},
+                   origin=xc.tolist(), spacing=d.tolist())
+        
+    else:
+        if name is None:
+            print('For arr of type np.array or dict, you need to specify a name')
+            return
+        
+        if type(arr) is type(dict()):
+            imageToVTK(output_dir+sim.index+'_'+name,
+                       cellData=arr,
+                       origin=xc.tolist(), spacing=d.tolist())
+        
+        elif type(arr) is type(np.array([])):
+            print(arr.shape)
+            imageToVTK(output_dir+sim.index+'_'+name,
+                       cellData={name: arr},
+                       origin=xc.tolist(), spacing=d.tolist())
+            
+        else:
+            print('arr must be of type str, np.array or dict')
+            
